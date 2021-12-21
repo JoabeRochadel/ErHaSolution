ef7a0a2e12c10e867f50771c2233e6fc7e5a1960
Joabe Rochadel <joaberochadel@gmail.com>
2021-12-21 10:49:03 -0300

    employee controller alteração add os verbos http

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,20 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace ErHaSolution.Controllers
 {
     [Route("employee")]
     public class EmployeeController : ControllerBase
     {
-        
-        public EmployeeController()
-        {
-            
-        }
 
-        [Route("/")]
-        public string Metodo()
+        //[Route("")]
+        //public string Metodo()
+        //{
+        //    return "oi";
+        //}
+
+        [HttpGet]
+        [Route("")]
+        public string Get()
         {
             return "oi";
         }
+
+        [HttpPost]
+        [Route("")]
+        public string Post()
+        {
+            return "oi";
+        }
+
+        [HttpPut]
+        [Route("")]
+        public string Put()
+        {
+            return "oi";
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public string Delete()
+        {
+            return "oi";
+        }
+
     }
 }
